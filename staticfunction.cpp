7bfed83a9c45a4f955c567be9afb2cccbac63b76
riswandaP <riswandaputra03@gmail.com>
2026-05-05 20:09:14 +0700

    Membuat prosedur staticfunction

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -39,3 +39,20 @@ void Mahasiswa::printAll() {
     cout << "Nama = " << nama << endl;
     cout << endl;
 }
+
+int main() {
+
+    Mahasiswa mhs1("Sri Dadi");
+    Mahasiswa mhs2("Budi Jatmiko");
+    Mahasiswa::setNim(9);
+    Mahasiswa mhs3("Andi Janu");
+    Mahasiswa mhs4("Joko Wahono");
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
+    system("pause");
+
+    return 0;
+}
