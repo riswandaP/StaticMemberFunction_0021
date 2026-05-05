50ef6e6c21405b999c7701705a9813ad0b3142de
riswandaP <riswandaputra03@gmail.com>
2026-05-05 20:08:28 +0700

    Membuat class mahasiswa static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -27,32 +27,3 @@ public:
     }
 
 };
-
-int Mahasiswa::nim = 0;
-
-void Mahasiswa::setID() {
-    id = ++nim;
-}
-
-void Mahasiswa::printAll() {
-    cout << "ID = " << id << endl;
-    cout << "Nama = " << nama << endl;
-    cout << endl;
-}
-
-int main() {
-
-    Mahasiswa mhs1("Sri Dadi");
-    Mahasiswa mhs2("Budi Jatmiko");
-    Mahasiswa::setNim(9);
-    Mahasiswa mhs3("Andi Janu");
-    Mahasiswa mhs4("Joko Wahono");
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-    cout << "Akses dari luar object = " << Mahasiswa::getNim() << endl;
-    system("pause");
-
-    return 0;
-}
