bbfc923f7b54dd69803c57a1ee106f978f73d70d
riswandaP <riswandaputra03@gmail.com>
2026-05-05 19:19:52 +0700

    Membuat class buku chainfunctioncall

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class buku{
+    string judul;
+
+    public:
+        buku &setJudul(string judul){
+            this->judul = judul;
+            return *this;
+        }
+        string getJudul(){
+            return this->judul;
+        }
+};
+
