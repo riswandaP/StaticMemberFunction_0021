255f44867576832ed48cf3183007f1ecc01c7493
riswandaP <riswandaputra03@gmail.com>
2026-05-05 20:07:12 +0700

    Membuat class mahasiswa static member

--- staticMember.cpp
+++ staticMember.cpp
@@ -1,3 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa {
+    public:
+        static int nim;
+
+        int id;
+        string nama;
+
+        void setID();
+        void printAll();
+
+        Mahasiswa(string pnama) : nama(pnama) {
+            setID();
+    }
+};
+
+int Mahasiswa::nim = 0;
+
+void Mahasiswa::setID() {
+    id = ++nim;
+};
+
