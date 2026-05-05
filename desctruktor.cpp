97168f3dd3f5c30227e582f57b57e51682849456
riswandaP <riswandaputra03@gmail.com>
2026-05-05 19:16:50 +0700

    Membuat fungsi isi data destructor

--- desctruktor.cpp
+++ desctruktor.cpp
@@ -35,3 +35,13 @@ void angka::cetakData()
     }
 }
 
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
